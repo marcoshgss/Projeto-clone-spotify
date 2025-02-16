80c66d02d3d68c16ede35521b3ef9fba64e79aa4
Marcos Henrique <sena_marcos.fp@hotmail.com>
2025-02-16 18:13:52 -0300

    ajustes banco de dados

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -4,7 +4,7 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://MarcosHenrique1:7Wc50m2NXj0FWRjl@cluster0.zknjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
